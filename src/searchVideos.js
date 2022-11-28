8c0eec5c851225e97db5741de3d492d52c8ac55f
Raed56 <rseraj@usf.edu>
2022-11-28 05:24:26 -0500

    added searchVideos function for api call

--- src/searchVideos.js
+++ src/searchVideos.js
@@ -1,8 +1,26 @@
 import React from "react";
 
 function SearchVideos() {
+    const searchVideos = async (e) => {
+        e.preventDefault(); //prevent it from actually posting the data by default
+        console.log("submitting");
+
+        const query = "golden retriever"; //hard coded for now
+
+        const url = `https://www.googleapis.com/youtube/v3/search?
+        key=AIzaSyAmLtGt_mZ3Pv-TWEeNxHdS-CEXJ-Ny6yc&part=snippet&type=video&q=dog+${query}`;
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchVideos}>
             <label className="label" htmlFor="query">
                 {" "}
                 Video Name
@@ -21,3 +39,4 @@ function SearchVideos() {
 }
 
 export default SearchVideos;
+//fix errors with api response
