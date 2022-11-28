7145b83263c8b2d3eece8f00d7d7ce0495981233
Raed56 <rseraj@usf.edu>
2022-11-28 05:29:39 -0500

    added state management for query

--- src/searchVideos.js
+++ src/searchVideos.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchVideos() {
+    const [query, setQuery] = useState("");
+
     const searchVideos = async (e) => {
         e.preventDefault(); //prevent it from actually posting the data by default
         console.log("submitting");
-
-        const query = "golden retriever"; //hard coded for now
 
         const url = `https://www.googleapis.com/youtube/v3/search?
         key=AIzaSyAmLtGt_mZ3Pv-TWEeNxHdS-CEXJ-Ny6yc&part=snippet&type=video&q=dog+${query}`;
@@ -30,6 +30,8 @@ function SearchVideos() {
                 type="text"
                 name="query"
                 placeholder="Dog Videos"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
             />
             <button className="button" type="submit">
                 Search Video
