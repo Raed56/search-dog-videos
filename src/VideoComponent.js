839703f890a06085d6d965e43d72ca46900f2e5f
Raed56 <rseraj@usf.edu>
2022-11-28 22:07:47 -0500

    included handle click and added params for video

---
+++ src/VideoComponent.js
@@ -0,0 +1,19 @@
+import SearchVideos from "./SearchVideos";
+
+function VideoComponent(videoId) {
+    return (
+        <div>
+            <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${videoId}`}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            ></iframe>
+        </div>
+    );
+}
+
+export default VideoComponent;
--- src/searchVideos.js
+++ src/searchVideos.js
@@ -1,13 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import VideoComponent from "./VideoComponent";
 
 function SearchVideos() {
     const [query, setQuery] = useState("");
 
     const [videos, setVideos] = useState([]);
 
-    const [sendRequest, setsendRequest] = useState(false);
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log("hereheehererer");
+        fetch(
+            `https://www.googleapis.com/youtube/v3/search?key=AIzaSyAmLtGt_mZ3Pv-TWEeNxHdS-CEXJ-Ny6yc&part=snippet&type=video&q=dog+${query}`
+        )
+            .then((data) => data.json())
+            .then((data) => {
+                console.log(data);
+                console.log(data.items);
+                setVideos(data.items);
+            });
+    };
 
-    useEffect(() => {
+    /*useEffect(() => {
         async function fetchdogData() {
             const url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyAmLtGt_mZ3Pv-TWEeNxHdS-CEXJ-Ny6yc&part=snippet&type=video&q=dog+${query}`;
 
@@ -16,14 +29,14 @@ function SearchVideos() {
                 const data = await response.json();
                 console.log(data);
                 console.log(data.results); //tests for videos
-                setVideos(data.results);
+                setVideos(data.results.items);
                 setsendRequest(false);
             } catch (err) {
                 console.error(err);
             }
         }
         fetchdogData();
-    }, [sendRequest]);
+    }, [sendRequest]);*/
 
     return (
         <>
@@ -40,29 +53,29 @@ function SearchVideos() {
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
                 />
-                <button
-                    className="button"
-                    type="submit"
-                    onClick={() => setsendRequest(true)}
-                >
+                <button className="button" type="submit" onClick={handleClick}>
                     Search Video
                 </button>
             </form>
             <div className="card-list">
                 {videos.map((video) => (
-                    <div className="card" key={video.id}>
+                    <div className="card" key={video.id.videoId}>
                         <img
                             className="card--image"
-                            src={`https://www.pexels.com/search/dog/${video.thumbnail}`} //src is random placeholder for now
-                            alt={video.title + "thumbnail"}
+                            src={`${video.snippet.thumbnails.medium.url}`} //src is random placeholder for now
+                            alt={video.snippet.title + "thumbnail"}
                         />
                         <div className="card--content">
-                            <h3 className="card--title">{video.title}</h3>
+                            <h3 className="card--title">
+                                {video.snippet.title}
+                            </h3>
                             <p>
-                                <small>Channel: {video.channel}</small>
+                                <small>
+                                    Channel Name: {video.snippet.channelTitle}
+                                </small>
                             </p>
                             <p className="card--desc">
-                                Description: {video.description}
+                                Video Description: {video.snippet.description}
                             </p>
                         </div>
                     </div>
